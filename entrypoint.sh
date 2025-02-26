d0179b1cd8b3354c512d218e304d0bb9f6dafa7e
Christopher Aubuchon <aubuchcl@gmail.com>
2025-02-26 07:44:16 -0800

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,17 @@ mkdir -p "$(dirname "$OUTPUT_FILE")"
 # Set the duration and interval
 duration=10   # Total duration in seconds
 interval=0.5  # Interval in seconds
-end_time=$((SECONDS + duration))
+start_time=$(date +%s)
 
 # Loop for the specified duration
-while [ $SECONDS -lt $end_time ]; do
+while true; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    
+    if [ "$elapsed_time" -ge "$duration" ]; then
+        break
+    fi
+
     cat /var/run/cycle/metadata/environment.json >> "$OUTPUT_FILE"
     echo "\n---\n" >> "$OUTPUT_FILE"  # Separator for readability
     sleep "$interval"
